1ee58832fbec4507d11e40c54625237f1941f2cb
1ambda <1amb4a@gmail.com>
2016-01-19 00:41:27 +0900

    fix: Rename profile-view to app-template #1

--- generator-omg-basic/generators/app/__spec__/index.spec.js
+++ generator-omg-basic/generators/app/__spec__/index.spec.js
@@ -1,11 +1,11 @@
 'use strict';
 var path = require('path');
 var assert = require('yeoman-assert');
-var helpers = require('yeoman-generator').test;
+var helpers = require('yeoman-test');
 
 describe('generator-omg-basic:app', function () {
   before(function (done) {
-    helpers.run(path.join(__dirname, '../app'))
+    helpers.run(path.join(__dirname, '../../app'))
       .withOptions({someOption: true})
       .withPrompts({someAnswer: true})
       .on('end', done);
--- generator-omg-basic/profile-app/tools/build.js
+++
@@ -1,8 +0,0 @@
-import task from './lib/task';
-
-export default task(async function build() {
-  await require('./clean')();
-  await require('./copy')();
-  await require('./bundle')();
-  await require('./render')();
-});
